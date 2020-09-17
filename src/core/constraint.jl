25fb5efd6bfa02e257a5cc712c3714425f1f6c4b
Marta Vanin <mvanin@esat.kuleuven.ac.be>
2020-09-17 16:07:04 +0200

    new string definition of se criteria

--- src/core/constraint.jl
+++ src/core/constraint.jl
@@ -9,7 +9,7 @@ function constraint_mc_residual(pm::_PMs.AbstractPowerModel, i::Int;
     _PMD.ref(pm, nw, :meas, i, "cmp") == :branch ? id = (cmp_id, _PMD.ref(pm,nw,:branch, cmp_id)["f_bus"], _PMD.ref(pm,nw,:branch,cmp_id)["t_bus"]) : id = _PMs.ref(pm, nw, :meas, i, "cmp_id")
     var = _PMs.var(pm, nw, _PMs.ref(pm, nw, :meas, i, "var"), id)
     dst = _PMD.ref(pm, nw, :meas, i, "dst")
-    rsc = _PMD.ref(pm, nw, :setting)["weight_rescaler"]
+    rsc = _PMD.ref(pm, nw, :se_settings)["weight_rescaler"]
     for c in _PMD.conductor_ids(pm; nw=nw)
         if typeof(dst[c]) == Float64
             JuMP.@constraint(pm.model,
@@ -18,22 +18,22 @@ function constraint_mc_residual(pm::_PMs.AbstractPowerModel, i::Int;
             JuMP.@constraint(pm.model,
                 res[c] == 0.0
             )
-        elseif typeof(dst[c]) == _DST.Normal{Float64} && _PMD.ref(pm, nw, :setting)["estimation_criterion"] != "mle"
-            if _PMD.ref(pm, nw, :setting)["estimation_criterion"] == "wls"
+        elseif typeof(dst[c]) == _DST.Normal{Float64} && _PMD.ref(pm, nw, :se_settings)["estimation_criterion"] != "mle"
+            if _PMD.ref(pm, nw, :se_settings)["estimation_criterion"] == "wls"
                 JuMP.@constraint(pm.model,
                     res[c] == (var[c]-_DST.mean(dst[c]))^2 / (_DST.std(dst[c]) * rsc)^2
                 )
-            elseif _PMD.ref(pm, nw, :setting)["estimation_criterion"] == "rwls"
+            elseif _PMD.ref(pm, nw, :se_settings)["estimation_criterion"] == "rwls"
                     JuMP.@constraint(pm.model,
                         res[c] * (_DST.std(dst[c]) * rsc)^2 >= (var[c]-_DST.mean(dst[c]))^2
                     )
-            elseif _PMD.ref(pm, nw, :setting)["estimation_criterion"] == "wlav"
+            elseif _PMD.ref(pm, nw, :se_settings)["estimation_criterion"] == "wlav"
                 μ= _DST.mean(dst[c])
                 σ = _DST.std(dst[c])
                 JuMP.@NLconstraint(pm.model,
                     res[c] == abs(var[c]-μ) / σ / rsc
                 )
-            elseif _PMD.ref(pm, nw, :setting)["estimation_criterion"] == "rwlav"
+            elseif _PMD.ref(pm, nw, :se_settings)["estimation_criterion"] == "rwlav"
                 JuMP.@constraint(pm.model,
                     res[c] >= (var[c]-_DST.mean(dst[c])) / _DST.std(dst[c]) / rsc
                 )
@@ -43,8 +43,8 @@ function constraint_mc_residual(pm::_PMs.AbstractPowerModel, i::Int;
             else
                 error("State estimation criterion not recognized")
             end
-        elseif (_PMD.ref(pm, nw, :setting)["estimation_criterion"] == "mle" && typeof(dst[c]) != Float64) || (_PMD.ref(pm, nw, :setting)["estimation_criterion"] == "mixed" && typeof(dst[c]) ∉ [Float64, _DST.Normal{Float64}])
-            distr(x) = - rsc[1] + rsc[2] * _DST.logpdf(dst[c],x)
+        elseif (_PMD.ref(pm, nw, :se_settings)["estimation_criterion"] == "mle" && typeof(dst[c]) != Float64) || (_PMD.ref(pm, nw, :se_settings)["estimation_criterion"] == "mixed" && typeof(dst[c]) ∉ [Float64, _DST.Normal{Float64}])
+            distr(x) = - rsc[2] + rsc[1] * _DST.logpdf(dst[c],x)
             grd(x) = _DST.gradlogpdf(dst[c],x)
             hes(x) = heslogpdf(dst[c],x)
             f = Symbol("df_",i,"_",c)
