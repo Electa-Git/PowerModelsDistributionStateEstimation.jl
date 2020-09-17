07a5e6eecedc51d0b1f2d047a8299534de22c3b0
Marta Vanin <mvanin@esat.kuleuven.ac.be>
2020-09-17 14:04:00 +0200

    new doc structure

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,24 @@
-using Documenter
-using PowerModelsDSSE
+using Documenter, PowerModelsDSSE
 
 makedocs(
     modules     = [PowerModelsDSSE],
     format      = Documenter.HTML(mathengine = Documenter.MathJax()),
     sitename    = "PowerModelsDSSE.jl",
     authors     = "Tom Van Acker, Marta Vanin",
-    pages       = [ "Home"              => "index.md",
-                    "Manual"            =>
-                        ["Getting Started" =>  "quickguide.md"  ,
-                        "Input Data Format"       => "input_data_format.md",
-                         "Mathematical Model"       => "math_model.md",
-                         "Measurement Conversion"   => "measurements.md",
-                         "State Estimation Criteria" => "se_criteria.md"]
-                  ]
+    pages       = [
+              "Home"    => "intro.md",
+              "Manual"  => [
+                            "Getting Started"                => "quickguide.md",
+                            "Input Data Format"              => "input_data_format.md",
+                            "Mathematical Model "            => "math_model.md",
+                            "Measurements and Conversions"   => "measurements.md",
+                            "State Estimation Criteria"      => "se_criteria.md",
+                            ],
+             "Library " => [
+                            "Power Flow Formulations"        => "formulations.md",
+                            "Problem Specifications"         => "problems.md",
+                           ],
+                 ]
 )
 
 deploydocs(
