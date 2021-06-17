d47cff7e3b1cec9832649248cb2cc397f2df5bb3
MartaVanin <marta.vanin@esat.kuleuven.be>
2021-06-17 14:20:13 +0200

    update rescaler

--- src/core/constraint.jl
+++ src/core/constraint.jl
@@ -30,11 +30,11 @@ function constraint_mc_residual(pm::_PMD.AbstractUnbalancedPowerModel, i::Int; n
             JuMP.@constraint(pm.model, res[idx] == 0.0)         
         elseif crit ∈ ["wls", "ls"] && isa(dst[idx], _DST.Normal)
             JuMP.@constraint(pm.model,
-                res[idx] == (var[c] - μ)^2 / σ^2 / rsc
+                res[idx] *rsc^2 == (var[c] - μ)^2 / σ^2 
             )
         elseif crit == "rwls" && isa(dst[idx], _DST.Normal)
             JuMP.@constraint(pm.model,
-                res[idx] * rsc * σ^2 >= (var[c] - μ)^2
+                res[idx] * rsc^2 * σ^2 >= (var[c] - μ)^2
             )
         elseif crit ∈ ["wlav", "lav"] && isa(dst[idx], _DST.Normal)
             JuMP.@NLconstraint(pm.model,
