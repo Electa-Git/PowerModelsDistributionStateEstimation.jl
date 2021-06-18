755298991cb4e33079e1791035af3bcdc1acbb20
Marta Vanin <marta.vanin@esat.kuleuven.be>
2021-06-18 11:48:34 +0200

    test adjustment for new rsc pt 2

--- test/mixed_measurements.jl
+++ test/mixed_measurements.jl
@@ -155,7 +155,7 @@
             _PMDSE.update_all_bounds!(data; v_min = 0.8, v_max = 1.2, pg_min=-1.0, pg_max = 1.0, qg_min=-1.0, qg_max=1.0, pd_min=-1.0, pd_max=1.0, qd_min=-1.0, qd_max=1.0 )
             # set se settings
             data["se_settings"] = Dict{String,Any}("criterion" => crit,
-                                               "rescaler" => 1)
+                                               "rescaler" => 10)
 
             # solve the state estimation
             se_result = _PMDSE.solve_mc_se(data, model, ipopt_solver)
@@ -237,7 +237,7 @@
             _PMDSE.update_all_bounds!(data; v_min = 0.8, v_max = 1.2, pg_min=-1.0, pg_max = 1.0, qg_min=-1.0, qg_max=1.0, pd_min=-1.0, pd_max=1.0, qd_min=-1.0, qd_max=1.0 )
             # set se settings
             data["se_settings"] = Dict{String,Any}("criterion" => crit,
-                                               "rescaler" => 1)
+                                               "rescaler" => 10)
 
             # solve the state estimation
             se_result = _PMDSE.solve_mc_se(data, model, ipopt_solver)
