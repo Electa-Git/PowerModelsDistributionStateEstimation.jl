a241aae3004160de25d8683088ee66843af4b32e
MartaVanin <marta.vanin@esat.kuleuven.be>
2023-06-23 17:51:39 +0200

    add "obj_scaling_factor" to test solver

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,23 +6,18 @@
 # State Estimation.                                                            #
 ################################################################################
 
-using PowerModelsDistributionStateEstimation
+import PowerModelsDistributionStateEstimation as _PMDSE
 
 # import pkgs
-import Distributions
+import Distributions as _DST
 import HDF5
 import Ipopt
-import Polynomials
-import PowerModels, PowerModelsDistribution
+import Polynomials as _Poly
+import PowerModels
+import PowerModelsDistribution as _PMD
 import Statistics
 #import SCS #removed while SDP tests are not active
 using Test
-
-# pkg const
-const _DST = Distributions
-const _PMD = PowerModelsDistribution
-const _PMDSE = PowerModelsDistributionStateEstimation
-const _Poly = Polynomials
 
 #network and feeder from ENWL for tests
 ntw, fdr = 4, 2
@@ -35,13 +30,14 @@ rm_transfo = true
 rd_lines   = true
 
 # set solvers
-ipopt_solver = optimizer_with_attributes(Ipopt.Optimizer,"max_cpu_time"=>300.0,
-                                                              "tol"=>1e-9,
-                                                              "print_level"=>0, "mu_strategy"=>"adaptive")
+ipopt_solver = _PMDSE.optimizer_with_attributes(Ipopt.Optimizer,"max_cpu_time" => 300.0,
+                                                         "obj_scaling_factor" => 1e3,
+                                                         "tol" => 1e-9,
+                                                         "print_level" => 0, 
+                                                         "mu_strategy" => "adaptive")
 
-# scs_solver = optimizer_with_attributes(SCS.Optimizer, "max_iters"=>20000, "eps"=>1e-5,
+# scs_solver = _PMDSE.optimizer_with_attributes(SCS.Optimizer, "max_iters"=>20000, "eps"=>1e-5,
 #                                                             "alpha"=>0.4, "verbose"=>0) #deactivated while SDP tests not active
-
 
 @testset "PowerModelsDistributionStateEstimation" begin
 
